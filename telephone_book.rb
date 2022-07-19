2b41ea5fd86c73b3ddadf88317f74ba04c39eac6
Vitória Moura <vim.saturnino@gmail.com>
2022-07-19 18:11:39 -0300

    chore: agenda menu set

---
+++ telephone_book.rb
@@ -0,0 +1,31 @@
+loop do 
+  puts "
+  Telephone Book\n
+  ------------------------------\n
+  Choose an option:\n
+  1 - Contacts\n
+  2 - Add Contact\n
+  3 - Seek Contact\n
+  4 - Edit Contact\n
+  5 - Delete Contact\n
+  0 - Go out :(\n
+  ------------------------------\n"
+
+  option = gets.chomp.to_i
+
+  case option 
+    when 0
+      puts 'See you later :)';
+      break
+    when 1
+      
+    when 2
+   
+    when 3
+    
+    when 4
+
+    when 5
+      
+  end
+end
